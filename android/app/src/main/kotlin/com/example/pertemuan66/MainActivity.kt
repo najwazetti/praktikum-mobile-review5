7f707a4d4536c7317c926d1f52d5f07cb6a4473e
najwazetti <rivanniaaritha@gmail.com>
2024-10-16 10:58:42 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/pertemuan66/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pertemuan66
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan66/pages/introduction_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nama Kalian',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.black87),
+          bodyMedium: TextStyle(color: Colors.black54),
+          bodySmall: TextStyle(color: Colors.black45),
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white60),
+          bodySmall: TextStyle(color: Colors.white54),
+        ),
+      ),
+      themeMode: ThemeMode.dark,
+      home: const IntroductionPage(),
+    );
+  }
+}
